38e300067fb942ca65b90104c4bcf28a6b8ce6b5
Satya sai kiran Pithani <spithani@eiu.edu>
2025-11-30 11:45:06 -0600

    Refactor predict_label function to clean up commented code and ensure proper output handling

--- toy3_json_eval/model.py
+++ toy3_json_eval/model.py
@@ -6,9 +6,10 @@ from pathlib import Path
 sentiment_pipeline = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
 
 def predict_label(text: str) -> str: # input example: "I love programming!", output example: "POSITIVE"
-    print(text)
-    result = sentiment_pipeline(text) # outptut example: {'label': 'POSITIVE', 'score': 0.9998}
-    print(result)
+    #print(text)
+    result = sentiment_pipeline(text)[0] # outptut example: {'label': 'POSITIVE', 'score': 0.9998}
+    #print(result)
+    return result['label']
 
 data_path = Path(__file__).parent / "data.json"
 
@@ -17,6 +18,7 @@ with open(data_path, "r") as f:
 
 for item in data:
     text = item['text']
+
     predict_label(text)    
 
   
